aba2bbe6e3d1a603a096a3770b22cd6de05f0f46
wen-jie.tseng <wen-jie.tseng@telecom-paris.fr>
2020-10-08 17:00:07 +0200

    create a repo with imported projects so that we can reduce the time of doing importing chores

---
+++ Assets/Scripts/PlayerControllerRAB.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerControllerRAB : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+    public TextMeshProUGUI accText;
+    public bool isMobileBuild;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private int count;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+
+        if (isMobileBuild)
+        {
+            // enable reading sensor values
+            // InputSystem.EnableDevice(UnityEngine.InputSystem.Accelerometer.current);
+        }
+    }
+
+    void Update()
+    {
+        float dist = (this.transform.position - GameObject.Find("Ground").transform.position).magnitude;
+        if (dist > 30) this.transform.position = new Vector3(0, 1f, 0);
+    }
+
+    // void OnMove(InputValue movementValue)
+    // {
+    //     Vector2 movementVector = movementValue.Get<Vector2>();
+
+    //     movementX = movementVector.x;
+    //     movementY = movementVector.y;
+    // }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = Vector3.zero;
+        // if (isMobileBuild)
+        // {
+        //     // use accelarometer
+        //     // Android is right-handed coordinate, remember to change to Unity coordinate (left-handed) 
+        //     Vector3 a = UnityEngine.InputSystem.Accelerometer.current.acceleration.ReadValue();
+        //     accText.text = "Accelerometer: " + a.ToString("F6");
+        //     movement = new Vector3(a.x, 0.0f, a.y);
+        // }
+        // else
+        // {
+        //     movement = new Vector3(movementX, 0.0f, movementY);
+        // }
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickups")) 
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    // void OnCollisionEnter(Collider other)
+    // {
+    //     if (other.gameObject.CompareTag("wall"))
+    //     {
+    //         Handheld.Vibrate();
+    //     }
+    // }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 30)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/TiltingGround.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+// using UnityEngine.InputSystem;
+
+public class TiltingGround : MonoBehaviour
+{
+    public bool isUseOldInput;
+    public TextMeshProUGUI gyroText;
+    private Quaternion initialQuaternion;
+
+    void Start()
+    {
+        if (isUseOldInput)
+        {
+            // old input
+            UnityEngine.Input.gyro.enabled = true;
+            initialQuaternion = UnityEngine.Input.gyro.attitude;
+        }
+        // else
+        // {
+        //     // input system
+        //     InputSystem.EnableDevice(UnityEngine.InputSystem.AttitudeSensor.current);
+        //     initialQuaternion = UnityEngine.InputSystem.AttitudeSensor.current.attitude.ReadValue();
+        // }
+    }
+
+    void Update()
+    {
+        Quaternion q_sensor = Quaternion.identity;
+        if (isUseOldInput)
+        {
+            q_sensor = UnityEngine.Input.gyro.attitude;
+        }
+        // else
+        // {
+        //     q_sensor = UnityEngine.InputSystem.AttitudeSensor.current.attitude.ReadValue(); 
+        // }
+
+        this.transform.rotation = GyroToUnity(q_sensor);        
+        gyroText.text = (this.transform.rotation).eulerAngles.ToString("F6");
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        // change from right-handed coordinate to left-handed coordinate
+        return new Quaternion(-q.x, -q.z, -q.y, q.w);
+    }
+}
